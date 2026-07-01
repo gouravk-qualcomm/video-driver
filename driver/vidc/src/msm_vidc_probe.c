2f1b6644cae7ed9259a5abb31d98094efc091fde
Vishnu Reddy <busanna.reddy@oss.qualcomm.com>
2026-07-01 18:02:08 +0530

    iris: enable probe support for qcom,glymur-iris
    Register the "qcom,glymur-iris" compatible string so that driver can
    match and initialize glymur devices during probe.
    
    Signed-off-by: Vishnu Reddy <busanna.reddy@oss.qualcomm.com>

--- driver/vidc/src/msm_vidc_probe.c
+++ driver/vidc/src/msm_vidc_probe.c
@@ -70,7 +70,8 @@ static inline bool is_video_device(struct device *dev)
 		of_device_is_compatible(dev->of_node, "qcom,sa8775p-iris") ||
 		of_device_is_compatible(dev->of_node, "qcom,qcs8300-iris") ||
 		of_device_is_compatible(dev->of_node, "qcom,sc7280-venus") ||
-		of_device_is_compatible(dev->of_node, "qcom,chora-vidc"));
+		of_device_is_compatible(dev->of_node, "qcom,chora-vidc") ||
+		of_device_is_compatible(dev->of_node, "qcom,glymur-iris"));
 }
 
 static inline bool is_video_context_bank_device_node(struct device_node *of_node)
@@ -179,6 +180,7 @@ static const struct of_device_id msm_vidc_dt_match[] = {
 	{.compatible = "qcom,sa8775p-iris"},
 	{.compatible = "qcom,qcs8300-iris"},
 	{.compatible = "qcom,sc7280-venus"},
+	{.compatible = "qcom,glymur-iris"},
 	{.compatible = "qcom,vidc,cb-ns-pxl"},
 	{.compatible = "qcom,vidc,cb-ns"},
 	{.compatible = "qcom,vidc,cb-ns-bitstream"},
